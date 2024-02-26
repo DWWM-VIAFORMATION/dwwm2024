2a6a18423c1b02879a1ee4fcaae2e40fc728c1e3
Thierry BRU <thierry-bru@laposte.net>
2024-02-26 10:47:51 +0100

    Co-authored-by: EcharpeBleue <EcharpeBleue@users.noreply.github.com> Co-authored-by: Doua200 <Doua200@users.noreply.github.com> Co-authored-by: Fu1Yang <Fu1Yang@users.noreply.github.com> Co-authored-by: LAFORME-leo <LAFORME-leo@users.noreply.github.com>

---
+++ php/app/s4-objet/index.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'vendor/autoload.php';
+use Aventures\Personnage;
+require_once 'src/personnage.php';
+
+
+$frodon = new Aventures\Personnage();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
